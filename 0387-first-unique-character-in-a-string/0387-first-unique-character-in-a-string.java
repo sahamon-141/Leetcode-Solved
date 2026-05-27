5614be2873ec619e933ec7a0697a833ab96a1ae2
sahamon-141 <sahajitmondal07@gmail.com>
2026-05-27 23:04:12 +0530

    Time: 6 ms (85.3%), Space: 46.9 MB (69.24%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.java
@@ -1,5 +1,6 @@
 class Solution {
     public int firstUniqChar(String s) {
+        /*
         HashMap<Character, Stack<Integer>> map = new HashMap<>();
         
         for (int i = 0; i < s.length(); i++) {
@@ -21,5 +22,19 @@ class Solution {
         }
         
         return firstIndex == Integer.MAX_VALUE ? -1 : firstIndex;
+        */
+        int[] freq = new int[26];
+        
+        for (char c : s.toCharArray()) {
+            freq[c - 'a']++;
+        }
+        
+        for (int i = 0; i < s.length(); i++) {
+            if (freq[s.charAt(i) - 'a'] == 1) {
+                return i;
+            }
+        }
+        
+        return -1;
     }
 }
