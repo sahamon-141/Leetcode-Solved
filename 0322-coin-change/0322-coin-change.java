bf2fa0b19f92337b5281d1613a0ac66468e334c6
sahamon-141 <sahajitmondal07@gmail.com>
2026-08-14 00:54:22 +0530

    Time: 15 ms (82.95%), Space: 46.1 MB (84.44%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+
+class Solution {
+    public int coinChange(int[] coins, int amount) {
+      
+        if (amount == 0) return 0;
+        
+        int max = amount + 1;
+        int[] dp = new int[amount + 1];
+        Arrays.fill(dp, max);
+        
+    
+        dp[0] = 0;
+        
+        
+        for (int i = 1; i <= amount; i++) {
+            for (int coin : coins) {
+                if (coin <= i) {
+                    dp[i] = Math.min(dp[i], dp[i - coin] + 1);
+                }
+            }
+        }
+        
+        return dp[amount] > amount ? -1 : dp[amount];
+    }
+}
